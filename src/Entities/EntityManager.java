e6da5491b99abccf31022c2f4c09c6b9bf112c40
EitanSomething <eitant13@gmail.com>
2020-05-18 12:29:10 -0400

    First Commit

---
+++ src/Entities/EntityManager.java
@@ -0,0 +1,12 @@
+package Entities;
+
+import java.util.logging.Handler;
+
+public class EntityManager {
+    private Handler handler;
+
+
+    public EntityManager(Handler handler){
+        this.handler = handler;
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,80 @@
+import Window.CubeWindow;
+
+public class Game implements Runnable{
+
+    private CubeWindow display;
+    private int width, height;
+    public String title;
+
+    private boolean running = false;
+    private Thread thread;
+
+    public Game(String title, int width, int height){
+        this.width = width;
+        this.height = height;
+        this.title = title;
+    }
+    private void init(){
+        display = new CubeWindow(title, width, height);
+    }
+    public void run(){
+
+        init();
+
+        int fps = 60;
+        double timePerTick = 1000000000 / fps;
+        double delta = 0;
+        long now;
+        long lastTime = System.nanoTime();
+        long timer = 0;
+        int ticks = 0;
+
+        while(running){
+            now = System.nanoTime();
+            delta += (now - lastTime) / timePerTick;
+            timer += now - lastTime;
+            lastTime = now;
+
+            if(delta >= 1){
+
+                ticks++;
+                delta--;
+            }
+
+            if(timer >= 1000000000){
+                System.out.println("Ticks and Frames: " + ticks);
+                ticks = 0;
+                timer = 0;
+            }
+        }
+
+        stop();
+
+    }
+
+
+    public int getWidth(){
+        return width;
+    }
+    public int getHeight(){
+        return height;
+    }
+
+    public synchronized void start(){
+        if(running)
+            return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    public synchronized void stop(){
+        if(!running)
+            return;
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
